dce93fcc922ef1b92ae0c65eca4761b95bf82508
NgokNgok04 <117185466+NgokNgok04@users.noreply.github.com>
2024-11-23 16:54:04 +0700

    fix: bcrypt

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,47 +1,53 @@
-import express, {Request, Response} from "express";
-import jwt from "jsonwebtoken";
+import express, { Request, Response } from "express";
+// import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt";
+// const bcrypt = require("bcrypt");
 
 const app = express();
 
-//taruh index.ts dulu nanti dirubah kebanyakan contoh di index.ts soalnya 
+//taruh index.ts dulu nanti dirubah kebanyakan contoh di index.ts soalnya
 
 app.use(express.json());
-// ini sementara dulu db belum tau cara make
-const users: { username: string; email: string; password: string; name: string }[] = [];
+
+const users: {
+  username: string;
+  email: string;
+  password: string;
+  name: string;
+}[] = [];
 
 app.get("/api", (req, res) => {
   res.json({ test: ["berto", "matthew", "indra", "hals"] });
 });
 
-
 app.post("/api/signup", async (req: Request, res: Response) => {
   const { username, email, name, password, confirmPassword } = req.body;
   if (!username || !email || !password || !name || !confirmPassword) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
-  else if (password !== confirmPassword) {
-    return res.status(400).json({ message: "Passwords do not match" });
-  }
-  else if (users.find((user) => user.email === email)) {
-    return res.status(400).json({ message: "Email already exists" });
+    res.status(400).json({ message: "All fields are required" });
+    return;
+  } else if (password !== confirmPassword) {
+    res.status(400).json({ message: "Passwords do not match" });
+    return;
+  } else if (users.find((user) => user.email === email)) {
+    res.status(400).json({ message: "Email already exists" });
+    return;
   }
 
   try {
-    // belum hashed password, library gak jelas cik
-    users.push({ username, email, password, name });
-    return res.status(201).json({ message: "User created successfully" });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({ username, email, password: hashedPassword, name });
+    res.status(201).json({ message: "User created successfully" });
+    return;
   } catch (error) {
-    return res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error" });
+    return;
   }
 });
 
-
-
-app.get("/api/login", (req, res) =>{
-  //TODO 
+app.get("/api/login", (req, res) => {
+  //TODO
 });
-
-
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000...");
