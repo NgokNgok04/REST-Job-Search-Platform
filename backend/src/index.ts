355e0d8ddccea34feb1dd75c1dd72ffa82275ad5
InTheRain <indrswaragalihjayanegara@gmail.com>
2024-11-23 14:59:59 +0700

    feat: init loginPage and registerPage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+// import  
 const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ test: ["berto", "matthew", "indra", "hals"] });
 });
+
+app.get("/api/signup", (req, res) => {
+  res.json({ message: "User signed up" });
+});
+
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000...");
---
+++ frontend/src/components/LoginPage.tsx
@@ -0,0 +1,52 @@
+// import React from "react";
+import { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const handleLogin = async () => {
+        try{
+            const response = await fetch("http://localhost:3000/api/login", {
+                method: "POST",
+                headers:{
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                throw new Error(data.message);
+            }
+        } catch (err: any){
+            setError(err);
+        }
+    }
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="flex flex-col p-6 border rounded shadow-lg">
+                <h2 className="text-2xl mb-4">Login</h2>
+                <input 
+                    type="email" 
+                    placeholder="mail@gmail.com" 
+                    className="mb-2 p-2 border rounded-sm"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password" 
+                    placeholder="password"
+                    className="mb-2 p-2 border rounded-sm"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleLogin}>Login</button>
+                {error && <p className="text-red-500 mt-2">{error}</p>}
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
